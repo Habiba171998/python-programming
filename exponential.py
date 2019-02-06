17961e26e444be675dab9445eb40eb015f79cf05
Habiba171998 <37238392+Habiba171998@users.noreply.github.com>
2019-02-06 10:13:13 +0530

    Update exponential.py

--- exponential.py
+++ exponential.py
@@ -1,8 +1,8 @@
-def power(base,exp):
-    if(exp==1):
-        return(base)
-    if(exp!=1):
-        return(base*power(base,exp-1))
-base=int(input())
-exp=int(input())
-print(power(base,exp))
+def power(num1,num2):
+    if(num2==1):
+        return(num1)
+    if(num2!=1):
+        return(num1*num2(num1,num2-1))
+num1=int(input())
+num2=int(input())
+print(power(num1,num2))
